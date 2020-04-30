ce7bf78cd733ae914bc16b6d148c25edb0bf4953
DhanushM12 <dhanushmukhi@gmail.com>
2020-04-30 16:10:42 +0530

    Change in options route

--- routes/options.js
+++ routes/options.js
@@ -2,13 +2,13 @@ const express = require("express"); //fetch the existing instance
 
 const router = express.Router(); //calling the Router
 
-const questionApi = require("../controllers/options_controller"); //getting the options controller
+const optionsApi = require("../controllers/options_controller"); //getting the options controller
 
 //to delete an option
-router.post("/:id/delete", questionApi.deleteOption);
+router.post("/:id/delete", optionsApi.deleteOption);
 
 //to increment the count of votes
-router.post("/:id/add_vote", questionApi.addVote);
+router.post("/:id/add_vote", optionsApi.addVote);
 
 //exporting the router
 module.exports = router;
